c374ecb94af5f4f849539169efc8abaa9f9e7a23
kimjeyoun <wodbs5602@naver.com>
2025-10-13 10:04:27 +0900

    ui: energy page 초안 완성 #6

---
+++ src/components/solution/List.tsx
@@ -0,0 +1,54 @@
+import { Box, List as MuiList, ListItem, Typography } from "@mui/material";
+import type { ListProps } from "../../types/solution";
+
+const List = (listProps: ListProps) => {
+  const { title, contents } = listProps;
+  return (
+    <Box sx={{ mt: 8 }}>
+      {/* 제목 */}
+      <Typography
+        sx={{
+          fontFamily: "Freesentation-7-Bold",
+          fontSize: "16px",
+          fontWeight: "bold",
+        }}
+      >
+        {title}
+      </Typography>
+
+      {/* Q&A 목록 */}
+      <MuiList sx={{ width: "100%" }}>
+        {contents.map((content, index) => (
+          <ListItem
+            key={index}
+            sx={{
+              display: "block", // 기본적으로 flex이기 때문에 block으로 변경
+              py: 1.5, // 기본 패딩 제거하고 커스텀
+              px: 0,
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: "Freesentation-6-SemiBold",
+                fontSize: "14px",
+                mb: 0.5,
+              }}
+            >
+              {content.title}
+            </Typography>
+            <Typography
+              sx={{
+                fontFamily: "Freesentation-5-Medium",
+                fontSize: "13px",
+              }}
+            >
+              {content.contents}
+            </Typography>
+          </ListItem>
+        ))}
+      </MuiList>
+    </Box>
+  );
+};
+
+export default List;
