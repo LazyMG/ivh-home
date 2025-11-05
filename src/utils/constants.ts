fa957bfee0629f3e886b654f9d738690a90e2511
LazyMG <cbfmark@gmail.com>
2025-11-05 09:27:42 +0900

    feat: 추가된 API 엔드 포인트 추가 - send-product-info #66

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -19,6 +19,9 @@ export const API_ENDPOINTS = {
     SEARCH_BY_TAG: (tag: string) => `/news/tag/${tag}`,
     DETAIL: (id: number) => `/news/${id}`,
   },
+  CUSTOMER: {
+    SEND_PRODUCT_INFO: "/customers/send-product-info",
+  },
 } as const;
 
 // 달력에 사용되는 색상을 객체로 분리함
@@ -31,7 +34,8 @@ export const RESERVATION_STATUS_COLOR: ReservationStatusColorType = {
 export const EAMIL_REGEX =
   /^(?!\.)(?!.*\.\.)([a-z0-9_'+\-.]*)[a-z0-9_+-]@([a-z0-9][a-z0-9-]*\.)+[a-z]{2,}$/i;
 
-export const PHONE_REGEX = /^(02-\d{3,4}-\d{4}|0[3-9]\d-\d{3,4}-\d{4}|01[016789]-\d{3,4}-\d{4})$/;
+export const PHONE_REGEX =
+  /^(02-\d{3,4}-\d{4}|0[3-9]\d-\d{3,4}-\d{4}|01[016789]-\d{3,4}-\d{4})$/;
 
 export const RESERVATION_TYPE: ReservationTypeMap = {
   EDUCATION: "교육",
