77f91df30475655a91dfc8a30882c65f3fe3b407
LazyMG <cbfmark@gmail.com>
2025-11-21 13:53:01 +0900

    deploy: 임시 base url 변경

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,9 @@ import type {
   ReservationTypeMap,
 } from "../types/reservation";
 
-export const API_BASE_URL = import.meta.env.VITE_BASE_API_URL;
+// export const API_BASE_URL = import.meta.env.VITE_BASE_API_URL;
+
+export const API_BASE_URL = "https://lazymg.com:8443/api";
 
 export const API_ENDPOINTS = {
   MENU: {
