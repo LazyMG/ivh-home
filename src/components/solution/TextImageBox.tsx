b7770235e442b86574e0ff84459b636996ec72e1
LazyMG <cbfmark@gmail.com>
2025-12-05 18:03:59 +0900

    fix: 솔루션 내 페이지의 이미지 가운데 정렬로 수정

--- src/components/solution/TextImageBox.tsx
+++ src/components/solution/TextImageBox.tsx
@@ -69,6 +69,7 @@ const TextImageBox = (textImageBoxProps: TextImageBoxProps) => {
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
+              width: "100%",
             }}
             key={index}
           >
