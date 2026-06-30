36fa8c3351eefc0b16f0adbba703aa7448638914
LazyMG <cbfmark@gmail.com>
2026-06-30 09:37:16 +0900

    feat: 프리뷰 배포를 위한 임시 수정 #203

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import { Box, CssBaseline, Skeleton, Typography } from "@mui/material";
 import SEO from "../common/SEO";
 import { Fragment, lazy, Suspense, useEffect, useState } from "react";
 import dayjs from "dayjs";
-import { newsService } from "../service/newsService";
+// import { newsService } from "../service/newsService";
 import type { NewsResponse } from "../service/newsService";
 
 import ContactTrainingInfoSection from "../components/home/ContactTrainingInfoSection";
@@ -105,10 +105,11 @@ const Home = () => {
   const fetchNews = async () => {
     setNewsStatus("loading");
     try {
-      const response = await newsService.getNews();
+      // const response = await newsService.getNews();
       // 임시 데이터를 DB 데이터 앞에 끼운 뒤, 앞에서 4개만 잘라서 표시
-      setNewsItems([...TEMP_NEWS_ITEMS, ...response].slice(0, 4));
+      // setNewsItems([...TEMP_NEWS_ITEMS, ...response].slice(0, 4));
       // setNewsItems([...response].slice(0, 4));
+      setNewsItems(TEMP_NEWS_ITEMS);
       setNewsStatus("success");
     } catch {
       setNewsItems([]);
